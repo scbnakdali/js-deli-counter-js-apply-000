43e5f0936a09a8683f969cf180b5df827e4102a2
Sarah Bullard Nakdali <scadmusbullard@gmail.com>
2019-08-08 16:13:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ function takeANumber(katzDeliLine, name) {
   return 'Welcome, ' + name + '. You are number ' + katzDeliLine.length + ' in line.';
 }
 
+function takeANumber(katzDeliLine) {
+  katzDeliLine
+}
+
 function currentLine(katzDeliLine) {
   if (katzDeliLine.length === 0) 
   { return 'The line is currently empty.';
